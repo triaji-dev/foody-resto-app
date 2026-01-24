fb22a62d41adff865709ef7326a4cbc2719e9f9d
nnndaru <aji.prabandaru@gmail.com>
2026-01-24 11:19:22 +0700

    fix setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
 import Providers from '@/store/providers';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nunito = Nunito({
+  variable: '--font-nunito',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
 });
 
 export const metadata: Metadata = {
-  title: "Restaurant App",
-  description: "Order your favorite food online",
+  title: 'Foody',
+  description: 'Order your favorite food online',
 };
 
 export default function RootLayout({
@@ -24,13 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunito.variable} font-sans antialiased`}
+        suppressHydrationWarning={true}
       >
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <div>
-Home
-    </div>
-  );
+  return <div className='display-3xl-bold'>Apakah font ini Nunito?</div>;
 }
